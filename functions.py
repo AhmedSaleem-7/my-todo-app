25565822be49cae8c4b842d5d94207268665ca3e
Ahmed Saleem <ahmsalc1234@gmail.com>
2025-12-23 21:13:50 +0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath = FILEPATH):
+    """ Read a text file and return a
+    list of the todos.
+    """
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath = FILEPATH):
+    """ Write the todo items to a text file."""
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todos_arg)
+
